cb36c07e5c7d5a0112a0952dc77921eb8c83b8f4
ismaklinaku <115666323+ismaklinaku@users.noreply.github.com>
2024-11-09 00:46:05 +0100

    Update client.cpp

---
+++ Rrjeta_Kompjuterike_TCP-Socket_Gr6/client.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+
+#pragma comment(lib, "Ws2_32.lib")
+
+using namespace std;
+
+#define SERVER_NAME = ''
+#define PORT 8080
+
+#define BUFFER_SIZE 1024
